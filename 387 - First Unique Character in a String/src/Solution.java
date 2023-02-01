075686895bdfa7d9925a4b9c0b2622e656fa0c80
Jaskaran Singh <jsingh2250@gmail.com>
2023-01-31 19:59:30 -0500

    Added improved solution for LeetCode problem 387.

--- 387 - First Unique Character in a String/src/Solution.java
+++ 387 - First Unique Character in a String/src/Solution.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
 // Constraints:
@@ -8,24 +6,16 @@ import java.util.HashMap;
 
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> firstIndexByCharacter = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char currentCharacter = s.charAt(i);
-
-            if (firstIndexByCharacter.containsKey(currentCharacter)) {
-                firstIndexByCharacter.put(currentCharacter, s.length());
-            }
-            else {
-                firstIndexByCharacter.put(currentCharacter, i);
-            }
-        }
-
         int indexOfFirstNonRepeatingCharacter = s.length();
-        for (int index : firstIndexByCharacter.values()) {
-            indexOfFirstNonRepeatingCharacter = Math.min(index, indexOfFirstNonRepeatingCharacter);
+
+        for (char character = 'a'; character <= 'z'; character++) {
+
+            int firstIndexOfCharacter = s.indexOf(character);
+            if ((firstIndexOfCharacter != -1) && (firstIndexOfCharacter == s.lastIndexOf(character))) {
+                indexOfFirstNonRepeatingCharacter = Math.min(indexOfFirstNonRepeatingCharacter, firstIndexOfCharacter);
+            }
         }
 
-        return (indexOfFirstNonRepeatingCharacter == s.length()) ? -1 : indexOfFirstNonRepeatingCharacter;
+        return ((indexOfFirstNonRepeatingCharacter == s.length()) ? -1 : indexOfFirstNonRepeatingCharacter);
     }
 }
